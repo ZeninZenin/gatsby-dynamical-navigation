87bc22f297d0c31f109e35994b9e8a0b5ebfc310
Sergey Zenin <Sergey_Zenin@epam.com>
2019-09-05 10:56:04 +0300

    feat: add package API types declaration - add `index.d.ts`

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react';
+
+interface ChildSiteNavigation extends NavigationLink {
+  order?: number;
+  fields: {
+    isRoot: boolean;
+  } | null;
+}
+
+interface NavigationLink {
+  path: string;
+  title: string;
+}
+
+interface NavigationNode extends NavigationLink {
+  childrenSiteNavigation: ChildSiteNavigation[];
+}
+
+type CurrentNavigation = NavigationNode[] | null;
+
+interface NavigationProps {
+  root: string;
+  target: string;
+  loader?: ReactNode;
+}
+
+declare module 'gatsby-dynamical-navigation' {
+  export const Navigation: React.FC<NavigationProps>;
+  export function loadNavigation(callback: () => void, isNavNotLoaded?: boolean): CurrentNavigation;
+}
